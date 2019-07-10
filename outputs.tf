87c20ba41f611536b9bdd9c16c4d3d5617ec1bbe
David McPike <david.mcpike@clearesult.com>
2019-07-09 20:13:02 -0500

    Add more outputs and update test

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,14 @@ output "name" {
   value = azurerm_storage_account.storageaccount.*.name
 }
 
+output "primary_location" {
+  value = azurerm_storage_account.storageaccount.*.primary_location
+}
+
+output "secondary_location" {
+  value = azurerm_storage_account.storageaccount.*.secondary_location
+}
+
 output "StorageBlobDataContributorId" {
   value = azuread_group.StorageBlobDataContributor.id
 }
@@ -20,5 +28,73 @@ output "StorageBlobDataContributorName" {
 
 output "StorageAccountKeyOperatorServiceRoleName" {
   value = azuread_group.StorageAccountKeyOperatorServiceRole.name
+}
+
+output "account_kind" {
+  value = azurerm_storage_account.storageaccount.*.account_kind
+}
+
+output "account_tier" {
+  value = azurerm_storage_account.storageaccount.*.account_tier
+}
+
+output "account_replication_type" {
+  value = azurerm_storage_account.storageaccount.*.account_replication_type
+}
+
+output "access_tier" {
+  value = azurerm_storage_account.storageaccount.*.access_tier
+}
+
+output "enable_blob_encryption" {
+  value = azurerm_storage_account.storageaccount.*.enable_blob_encryption
+}
+
+output "enable_file_encryption" {
+  value = azurerm_storage_account.storageaccount.*.enable_file_encryption
+}
+
+output "enable_https_traffic_only" {
+  value = azurerm_storage_account.storageaccount.*.enable_https_traffic_only
+}
+
+output "primary_blob_endpoint" {
+  value = azurerm_storage_account.storageaccount.*.primary_blob_endpoint
+}
+
+output "primary_blob_host" {
+  value = azurerm_storage_account.storageaccount.*.primary_blob_host
+}
+
+output "primary_queue_endpoint" {
+  value = azurerm_storage_account.storageaccount.*.primary_queue_endpoint
+}
+
+output "primary_queue_host" {
+  value = azurerm_storage_account.storageaccount.*.primary_queue_host
+}
+
+output "primary_table_endpoint" {
+  value = azurerm_storage_account.storageaccount.*.primary_table_endpoint
+}
+
+output "primary_table_host" {
+  value = azurerm_storage_account.storageaccount.*.primary_table_host
+}
+
+output "primary_file_endpoint" {
+  value = azurerm_storage_account.storageaccount.*.primary_file_endpoint
+}
+
+output "primary_file_host" {
+  value = azurerm_storage_account.storageaccount.*.primary_file_host
+}
+
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.storageaccount.*.primary_web_endpoint
+}
+
+output "primary_web_host" {
+  value = azurerm_storage_account.storageaccount.*.primary_web_host
 }
 
---
+++ test/fixture/outputs.tf
@@ -0,0 +1,100 @@
+output "id" {
+  value = module.storage.*.id
+}
+
+output "name" {
+  value = module.storage.*.name
+}
+
+output "primary_location" {
+  value = module.storage.*.primary_location
+}
+
+output "secondary_location" {
+  value = module.storage.*.secondary_location
+}
+
+output "StorageBlobDataContributorId" {
+  value = module.storage.StorageBlobDataContributorId
+}
+
+output "StorageAccountKeyOperatorServiceRoleId" {
+  value = module.storage.StorageAccountKeyOperatorServiceRoleId
+}
+
+output "StorageBlobDataContributorName" {
+  value = module.storage.StorageBlobDataContributorName
+}
+
+output "StorageAccountKeyOperatorServiceRoleName" {
+  value = module.storage.StorageAccountKeyOperatorServiceRoleName
+}
+
+output "account_kind" {
+  value = module.storage.*.account_kind
+}
+
+output "account_tier" {
+  value = module.storage.*.account_tier
+}
+
+output "account_replication_type" {
+  value = module.storage.*.account_replication_type
+}
+
+output "access_tier" {
+  value = module.storage.*.access_tier
+}
+
+output "enable_blob_encryption" {
+  value = module.storage.*.enable_blob_encryption
+}
+
+output "enable_file_encryption" {
+  value = module.storage.*.enable_file_encryption
+}
+
+output "enable_https_traffic_only" {
+  value = module.storage.*.enable_https_traffic_only
+}
+
+output "primary_blob_endpoint" {
+  value = module.storage.*.primary_blob_endpoint
+}
+
+output "primary_blob_host" {
+  value = module.storage.*.primary_blob_host
+}
+
+output "primary_queue_endpoint" {
+  value = module.storage.*.primary_queue_endpoint
+}
+
+output "primary_queue_host" {
+  value = module.storage.*.primary_queue_host
+}
+
+output "primary_table_endpoint" {
+  value = module.storage.*.primary_table_endpoint
+}
+
+output "primary_table_host" {
+  value = module.storage.*.primary_table_host
+}
+
+output "primary_file_endpoint" {
+  value = module.storage.*.primary_file_endpoint
+}
+
+output "primary_file_host" {
+  value = module.storage.*.primary_file_host
+}
+
+output "primary_web_endpoint" {
+  value = module.storage.*.primary_web_endpoint
+}
+
+output "primary_web_host" {
+  value = module.storage.*.primary_web_host
+}
+
